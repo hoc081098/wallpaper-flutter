e6632047b8735769dbc1d751aeda6fdcdb61eee1
Petrus Hoc <hoc081098@gmail.com>
2019-11-07 17:30:43 +0700

    up

--- lib/screens/downloads_page.dart
+++ lib/screens/downloads_page.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:tuple/tuple.dart';
 import 'package:wallpaper/data/database.dart';
 import 'package:wallpaper/data/models/downloaded_image.dart';
 
@@ -15,6 +16,8 @@ class DownloadedPage extends StatefulWidget {
 
 class _DownloadedPageState extends State<DownloadedPage> {
   static final dateFormatYMdHms = DateFormat.yMd().add_Hms();
+  static final dateFormatYMd = DateFormat.yMd();
+
   _DownloadedBloc bloc;
 
   @override
@@ -50,21 +53,45 @@ class _DownloadedPageState extends State<DownloadedPage> {
               itemCount: data.length,
               itemBuilder: (context, index) {
                 final item = data[index];
-                if (item is _ImageItem) {
-                  final image = item.image;
 
+                if (item is _HeaderItem) {
+                  final now = DateTime.now();
+                  final date = item.date;
+
+                  if (now.difference(date).inDays < 1) {
+                    return ListTile(
+                      title: Text(
+                        'Today',
+                        textScaleFactor: 1.1,
+                      ),
+                    );
+                  }
+                  if (now.difference(date).inDays < 2) {
+                    return ListTile(
+                      title: Text(
+                        'Yesterday',
+                        textScaleFactor: 1.1,
+                      ),
+                    );
+                  }
                   return ListTile(
-                    leading: CachedNetworkImage(
-                      imageUrl: image.imageUrl,
-                      fit: BoxFit.cover,
-                    ),
                     title: Text(
-                      image.name,
+                      dateFormatYMd.format(date),
+                      textScaleFactor: 1.1,
+                    ),
+                  );
+                }
+
+                if (item is _ImageItem) {
+                  return ListTile(
+                    leading: Image.file(item.imageFile),
+                    title: Text(
+                      item.name,
                       maxLines: 1,
                       overflow: TextOverflow.ellipsis,
                     ),
                     subtitle: Text(
-                      'Downloaded at: ${dateFormatYMdHms.format(image.createdAt)}',
+                      'Downloaded at: ${dateFormatYMdHms.format(item.createdAt)}',
                       textScaleFactor: 0.8,
                     ),
                     trailing: IconButton(
@@ -74,6 +101,8 @@ class _DownloadedPageState extends State<DownloadedPage> {
                     ),
                   );
                 }
+
+                return Container(width: 0, height: 0);
               },
             );
           }),
@@ -88,13 +117,21 @@ class _DownloadedPageState extends State<DownloadedPage> {
 abstract class _ListItem {}
 
 class _ImageItem implements _ListItem {
-  final DownloadedImage image;
+  final String id;
+  final String name;
+  final File imageFile;
+  final DateTime createdAt;
 
-  _ImageItem(this.image);
+  _ImageItem({
+    @required this.id,
+    @required this.name,
+    @required this.imageFile,
+    @required this.createdAt,
+  });
 }
 
 class _HeaderItem implements _ListItem {
-  final String date;
+  final DateTime date;
 
   _HeaderItem(this.date);
 }
@@ -115,15 +152,42 @@ class _DownloadedBloc {
   factory _DownloadedBloc() {
     final fetchS = PublishSubject<void>();
 
+    ///
+    ///
+    ///
+    Directory _cachedStorageDir;
+    getStorageDir() async {
+      if (_cachedStorageDir != null) {
+        return _cachedStorageDir;
+      }
+      if (Platform.isAndroid) {
+        _cachedStorageDir = await getExternalStorageDirectory();
+        return _cachedStorageDir;
+      }
+      if (Platform.isIOS) {
+        _cachedStorageDir = await getApplicationDocumentsDirectory();
+        return _cachedStorageDir;
+      }
+      throw StateError('Not yet support ${Platform.operatingSystem}');
+    }
+
+    ///
+    ///
+    ///
     final listItems$ = fetchS
         .switchMap((_) async* {
-          yield await ImageDB.getInstance().getDownloadedImages();
+          final images = await ImageDB.getInstance().getDownloadedImages();
+          final storageDirectory = await getStorageDir();
+          yield Tuple2(images, storageDirectory);
         })
         .map(_groupByDate)
         .publishValueSeeded(null);
 
     final connect = listItems$.connect();
 
+    ///
+    ///
+    ///
     return _DownloadedBloc._(
       () => fetchS.add(null),
       listItems$,
@@ -134,7 +198,42 @@ class _DownloadedBloc {
     );
   }
 
-  static List<_ListItem> _groupByDate(List<DownloadedImage> images) {
-    return [for (final image in images) _ImageItem(image)];
+  static List<_ListItem> _groupByDate(
+    Tuple2<List<DownloadedImage>, Directory> tuple,
+  ) {
+    final items = <_ListItem>[];
+
+    final images = tuple.item1;
+    final directory = tuple.item2;
+
+    DateTime prev;
+    for (final image in images) {
+      final createdAt = image.createdAt;
+      if (prev == null ||
+          (prev.year != createdAt.year ||
+              prev.month != createdAt.month ||
+              prev.day != createdAt.day)) {
+        final createdAtTrimHms = DateTime(
+          createdAt.year,
+          createdAt.month,
+          createdAt.day,
+        );
+
+        items.add(_HeaderItem(createdAtTrimHms));
+
+        prev = createdAtTrimHms;
+      }
+
+      items.add(
+        _ImageItem(
+          id: image.id,
+          imageFile: File(path.join(directory.path, image.imageUrl)),
+          createdAt: createdAt,
+          name: image.name,
+        ),
+      );
+    }
+
+    return items;
   }
 }
