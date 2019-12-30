561686c9b7d4432f24708d504181df47c95be7cd
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2019-12-30 16:10:12 +0700

    export apk

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/hoangtanduy/Desktop/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/hoangtanduy/Desktop/dev/wallpaper-flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hoangtanduy/Desktop/dev/clones/wallpaper-flutter"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
